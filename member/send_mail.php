403a983b923023e58722b21a995e27ee797df101
seahahn <seah.ahn.nt@gmail.com>
2021-01-03 21:54:03 +0900

    mailer fixed

--- member/send_mail.php
+++ member/send_mail.php
@@ -67,7 +67,7 @@ try {
     </script>";
 
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error : ", $mail -> ErrorInfo;
+    // echo "Message could not be sent. Mailer Error : ", $mail -> ErrorInfo;
 }
 
 function generateRandomString($length) {
