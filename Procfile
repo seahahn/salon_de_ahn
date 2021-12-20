52c9f147a45aecaf19e3045abf837b48b86afab5
seahahn <seah.ahn.nt@gmail.com>
2021-12-21 00:59:56 +0900

    heroku setting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: heroku-php-apache2 | cd /chatting/server | php websocket_server.php
--- db_con.php
+++ db_con.php
@@ -1,9 +1,13 @@
 <?php
-    $db_id="ahn";
-    $db_pw="SalonDeAhn930121!";
-    $db_name="salon_de_ahn";
+    // $db_id="ahn";
+    // $db_pw="SalonDeAhn930121!";
+    // $db_name="salon_de_ahn";
     // $db_domain="salondeahn.me";
-    $db_domain="localhost";
+
+    $db_id="h48k3s6czvuv4kkw";
+    $db_pw="rpx7h5svuakegvy3";
+    $db_name="tc8q2rjfpkbo9wlc";
+    $db_domain="dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     
     $db=mysqli_connect($db_domain,$db_id,$db_pw,$db_name);
 
