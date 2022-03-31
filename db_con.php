a569d639ae7cfa46b8e731cde36a2c53a73d29bc
seahahn <seah.ahn.nt@gmail.com>
2022-03-31 20:00:51 +0900

    gitignore updated

--- db_con.php
+++ db_con.php
@@ -1,8 +1,8 @@
 <?php
-    $db_id=$_SERVER['DB_USERID'];
-    $db_pw=$_SERVER['DB_PW'];
-    $db_name=$_SERVER['DB_NAME'];
-    $db_domain=$_SERVER['DB_HOST'];
+    $db_id=$_ENV['DB_USERID'];
+    $db_pw=$_ENV['DB_PW'];
+    $db_name=$_ENV['DB_NAME'];
+    $db_domain=$_ENV['DB_HOST'];
 
     $db=mysqli_connect($db_domain,$db_id,$db_pw,$db_name);
 
--- test/dbtest.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_con = mysqli_connect("salondeahn.me", "ahn", "SalonDeAhn930121!", "salon_de_ahn");
-if ($db_con){
-  echo "DB 연결 성공<p>";
-} else {
-  echo "DB 연결 실패<p>";
-}
-?>
