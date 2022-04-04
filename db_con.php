ef4f5879c246cc1e9b40f145f8dd7a98eb4e4c4c
seahahn <seah.ahn.nt@gmail.com>
2022-04-04 23:25:34 +0900

    db_con updated for env vars

--- db_con.php
+++ db_con.php
@@ -1,8 +1,8 @@
 <?php
-    $db_id=$_ENV['DB_USERID'];
-    $db_pw=$_ENV['DB_PW'];
-    $db_name=$_ENV['DB_NAME'];
-    $db_domain=$_ENV['DB_HOST'];
+    $db_id=getenv('DB_USERID');
+    $db_pw=getenv('DB_PW');
+    $db_name=getenv('DB_NAME');
+    $db_domain=getenv('DB_HOST');
 
     $db=mysqli_connect($db_domain,$db_id,$db_pw,$db_name);
 
